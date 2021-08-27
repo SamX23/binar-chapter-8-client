c6cc5ebd8a58b7dde7adc0a79f9b0a38032f72da
SamX23 <skalexsong@gmail.com>
2021-08-27 23:21:56 +0700

    Update : better layout

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -2,6 +2,16 @@ import { useEffect, useState } from "react";
 import { Card, Col, Row } from "react-bootstrap";
 
 const Dashboard = () => {
+  const styles = {
+    playerGrid: {
+      overflowY: "overlay",
+      maxHeight: "70vh",
+      display: "grid",
+      gridTemplateColumns: "repeat(auto-fit, minmax(15rem, 25rem))",
+      gap: "1rem",
+      justifyContent: "center",
+    },
+  };
   const [data, setData] = useState();
 
   useEffect(() => {
@@ -12,21 +22,23 @@ const Dashboard = () => {
   return (
     <Row>
       <Col>
-        <h3>Player List</h3>
-        {data ? (
-          data.map((item, key) => (
-            <Card key={key} className="my-1">
-              <Card.Body>
-                <p>Username : {item.username}</p>
-                <p>Email : {item.email}</p>
-                <p>Experience : {item.experience}</p>
-                <p>Level : {item.level}</p>
-              </Card.Body>
-            </Card>
-          ))
-        ) : (
-          <p>No data</p>
-        )}
+        <h3 className="text-center">PLAYER LIST</h3>
+        <div className="bg-dark rounded py-2" style={styles.playerGrid}>
+          {data ? (
+            data.map((item, key) => (
+              <Card key={key} className="my-1">
+                <Card.Body>
+                  <p>Username : {item.username}</p>
+                  <p>Email : {item.email}</p>
+                  <p>Experience : {item.experience}</p>
+                  <p>Level : {item.level}</p>
+                </Card.Body>
+              </Card>
+            ))
+          ) : (
+            <p>No data</p>
+          )}
+        </div>
       </Col>
     </Row>
   );
