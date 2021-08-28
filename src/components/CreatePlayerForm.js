80899633a9f88a641ade55c6528578cc8664388c
SamX23 <skalexsong@gmail.com>
2021-08-29 00:15:12 +0700

    Update : change form inline

--- src/components/CreatePlayerForm.js
+++ src/components/CreatePlayerForm.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Card, Form, Button } from "react-bootstrap";
+import { Card, Form, Button, FloatingLabel, Row, Col } from "react-bootstrap";
 
 const CreatePlayerForm = ({
   stateData,
@@ -45,64 +45,83 @@ const CreatePlayerForm = ({
       <Card.Title className="text-center py-3">Create Player</Card.Title>
       <Card.Body>
         <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicUsername">
-            <Form.Label>Username</Form.Label>
+          <FloatingLabel
+            controlId="formBasicUsername"
+            label="Username"
+            className="mb-3"
+          >
             <Form.Control
               type="text"
-              placeholder="Enter username"
+              placeholder="username"
               onChange={handleUsernameChange}
               value={stateData.username}
               minLength="5"
+              pattern="\S+"
               autoComplete="username"
               required
             />
-          </Form.Group>
+          </FloatingLabel>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
+          <FloatingLabel controlId="formBasicPassword" label="Password">
             <Form.Control
-              type="password"
-              placeholder="Password"
+              type="text"
+              placeholder="password"
               onChange={handlePasswordChange}
               value={stateData.password}
               minLength="5"
-              autoComplete="current-password"
+              pattern="\S+"
+              autoComplete="password"
               required
             />
-            <Form.Text className="text-muted">
-              We'll never share your password with anyone else.
-            </Form.Text>
-          </Form.Group>
+          </FloatingLabel>
+          <Form.Text className="text-muted mb-3">
+            We'll never share your password with anyone else.
+          </Form.Text>
 
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email</Form.Label>
+          <FloatingLabel
+            controlId="formBasicEmail"
+            label="Email"
+            className="my-3"
+          >
             <Form.Control
               type="email"
-              placeholder="Email"
+              placeholder="email"
               onChange={handleEmailChange}
               value={stateData.email}
             />
-          </Form.Group>
+          </FloatingLabel>
 
-          <Form.Group className="mb-3" controlId="formBasicExperience">
-            <Form.Label>Experience</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Experience"
-              onChange={handleExperienceChange}
-              value={stateData.experience}
-            />
-          </Form.Group>
+          <Row>
+            <Col>
+              <FloatingLabel
+                controlId="formBasicExperience"
+                label="Experience"
+                className="mb-3"
+              >
+                <Form.Control
+                  type="number"
+                  placeholder="2500"
+                  onChange={handleExperienceChange}
+                  value={stateData.experience}
+                />
+              </FloatingLabel>
+            </Col>
 
-          <Form.Group className="mb-3" controlId="formBasicLevel">
-            <Form.Label>Level</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Level"
-              onChange={handleLevelChange}
-              value={stateData.level}
-            />
-          </Form.Group>
+            <Col>
+              <FloatingLabel
+                controlId="formBasicLevel"
+                label="Level"
+                className="mb-3"
+              >
+                <Form.Control
+                  type="number"
+                  placeholder="99"
+                  onChange={handleLevelChange}
+                  value={stateData.level}
+                />
+              </FloatingLabel>
+            </Col>
+          </Row>
 
           <Button variant="primary" type="submit">
             Submit
