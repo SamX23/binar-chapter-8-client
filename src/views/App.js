8fbec4d927c70765eb68afaa99613ddda7483ace
SamX23 <skalexsong@gmail.com>
2021-08-29 22:39:47 +0700

    Update : refactor App folder

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import MainLayout from "./views/Layout";
-import Routing from "./router";
+import MainLayout from "./Layout";
+import Routing from "../router";
 
 export const App = () => (
   <Router>
